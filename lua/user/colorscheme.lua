c22fbb7319a4b751c791ed01ceb707bdb81bae28
Matt Noakes <mattnoakes@me.com>
2024-05-19 19:31:02 +0100

    catppuccin

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,16 @@
 local M = {
-  "LunarVim/darkplus.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  require('catppuccin').setup({
+    transparent_background = true,
+  })
+
+  vim.cmd.colorscheme "catppuccin-frappe"
 end
 
 return M
